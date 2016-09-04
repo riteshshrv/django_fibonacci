8be317d7c5cb3550dfc29a57afe13998fcf3b92a
Ritesh Shrivastav <ritesh_shrv@live.com>
2016-09-04 17:07:26 +0530

    Wrap functional tests with 'unittest'

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,26 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class FunctionalTest(unittest.TestCase):
+    def setUp(self):
+        # User Chrome as its the mostly used browser
+        self.browser = webdriver.Chrome()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_boilerplate(self):
+        self.browser.get('http://localhost:8000')
+
+        # Make sure the title has the app name
+        self.assertIn('Django', self.browser.title)
+
+        # There should be a input tag to enter 'N'
+
+        # Hitting Enter should make a POST request to get the Nth
+        # number and display it on page
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
 
