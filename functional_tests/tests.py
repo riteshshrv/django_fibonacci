593d7d5b137b11b56827ccae140a7f20c16885d4
Ritesh Shrivastav <ritesh_shrv@live.com>
2016-09-07 01:16:20 +0530

    Improve page layout using Bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
-class FunctionalTest(LiveServerTestCase):
+class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         # User Chrome as its the mostly used browser
         self.browser = webdriver.Chrome()
